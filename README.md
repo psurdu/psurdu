79492aed41fe22df03caf0f2f8e0acd1c96b60d5
psurdu <psurdu01@educantabria.es>
2021-12-13 13:46:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-4kq6zz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-4kq6zz)
---
+++ src/App.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Hola :),document.getElementById("root")</h1>);
