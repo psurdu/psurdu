c623be2228fef221a26360191fd2eb6c7717680f
psurdu <psurdu01@educantabria.es>
2021-12-15 13:10:42 +0100

    Listas con arrays

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { Lista } from './components/Lista.jsx';
 import { LineaLista } from './components/LineaLista.jsx';
 
 export default function App() {
+  const elementos = ['hola', 'hola2', 'hola3'];
+
   return (
     <div>
       <h1>Primer componente</h1>
-      <Lista nombre="Hola" />
+      <Lista nombre="Hola" elementos={elementos} />
+      <Lista nombre="Xd" elementos={elementos} />
     </div>
   );
 }
--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { LineaLista } from './LineaLista';
 
 export function Lista(props) {
+  const listaComp = [];
+
+  for (let i = 0; i < props.elementos.length; i++) {
+    listaComp.push(<LineaLista titulo={props.elementos[i]} />);
+  }
+
   return (
     <div>
       {props.nombre}
-      <ul>
-        <LineaLista titulo="t1" />
-        <LineaLista titulo="t2" />
-        <LineaLista titulo="t3" />
-      </ul>
+      <ul>{listaComp}</ul>
     </div>
   );
 }
