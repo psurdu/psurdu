4a7b0f2921e6abb0c06ef11897ba71cff969b1e4
psurdu <psurdu01@educantabria.es>
2022-01-17 13:13:52 +0100

    HEADER FUNCIONAL

--- src/components/BootstrapHeader.jsx
+++ src/components/BootstrapHeader.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 
+import { MenuItems } from '../data/MenuItems';
+
+import { Link } from 'react-router-dom';
+
 class BootstrapHeader extends React.Component {
   constructor(props) {
     super(props);
@@ -9,32 +13,47 @@ class BootstrapHeader extends React.Component {
   render() {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
-
-    );;
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              className="logo-image"
+              src="https://eu01.edcwb.com/buscador/img/centros/logogrande/7348-a9c730d6b2b644f5b9910364ba6af277.jpg"
+            />
+            <i className="fas fa-user-graduate" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse
+            id="responsive-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              {MenuItems.map((Item) => {
+                return (
+                  <Nav.Link key={Item.key} as={Link} to={Item.path}>
+                    {Item.title}
+                  </Nav.Link>
+                );
+              })}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
   }
 }
 
