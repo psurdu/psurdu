f095866285e2350830780c5ba61abc2f363ff173
psurdu <psurdu01@educantabria.es>
2022-01-18 14:07:02 +0100

    1 file modified

--- src/components/TablaDatos.jsx
+++ src/components/TablaDatos.jsx
@@ -1,5 +1,8 @@
+import getDocumentElement from '@popperjs/core/lib/dom-utils/getDocumentElement';
 import React from 'react';
 import { Table } from 'react-bootstrap';
+import { Container, Row, Card } from 'react-bootstrap';
+import Col from 'react-bootstrap/esm/Col';
 import { ListaDatos } from '../data/ListaDatos.js';
 
 class TablaDatos extends React.Component {
@@ -9,36 +12,43 @@ class TablaDatos extends React.Component {
 
   render() {
     return (
-      <Table responsive>
-        <thead>
-          <tr>
-            <th>#</th>
-            {ListaDatos.map((item) => (
-              <th key={item.id}>{item.heding}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            {ListaDatos.map((item) => (
-              <td key={item.id}>{item.cell1}</td>
-            ))}
-          </tr>
-          <tr>
-            <td>2</td>
-            {ListaDatos.map((item) => (
-              <td key={item.id}>{item.cell2}</td>
-            ))}
-          </tr>
-          <tr>
-            <td>3</td>
-            {ListaDatos.map((item) => (
-              <td key={item.id}>{item.cell3}</td>
-            ))}
-          </tr>
-        </tbody>
-      </Table>
+      <Container>
+        <Row>
+          <Col>
+            <Table responsive>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  {ListaDatos.map((item) => (
+                    <th key={item.id}>{item.heding}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  {ListaDatos.map((item) => (
+                    <td key={item.id}>{item.cell1}</td>
+                  ))}
+                </tr>
+                <tr>
+                  <td>2</td>
+                  {ListaDatos.map((item) => (
+                    <td key={item.id}>{item.cell2}</td>
+                  ))}
+                </tr>
+                <tr>
+                  <td>3</td>
+                  {ListaDatos.map((item) => (
+                    <td key={item.id}>{item.cell3}</td>
+                  ))}
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     );
   }
 }
